eb234d8f32f8b2b023a30c88dcd6f1d6275ecd34
sainadh-paruchuri <saiparuchuri121@gmail.com>
2022-12-01 13:41:21 +0530

    sql added

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db=require('./util/database')
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
